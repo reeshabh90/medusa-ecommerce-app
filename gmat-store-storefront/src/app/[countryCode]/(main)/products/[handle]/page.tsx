27e635681c61c854fecf0f708e13751b12fe779b
Reeshabh Choudhary <reeshabh.007@gmail.com>
2025-04-18 11:44:38 +0530

    Documentation

--- gmat-store-storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ gmat-store-storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -8,6 +8,14 @@ type Props = {
   params: Promise<{ countryCode: string; handle: string }>
 }
 
+/**
+ * Purpose: This function is used for Static Site Generation (SSG) with dynamic routes.
+   When it runs: During the build process, Next.js calls this function to determine all possible values for the dynamic route parameters ([countryCode] and [handle] in this case).
+   Why it's implemented:
+   It allows Next.js to pre-render pages for all the specified parameter combinations (e.g., /us/products/product-1, /uk/products/product-2).
+   This improves performance by serving pre-rendered HTML for these routes.
+ * @returns 
+ */
 export async function generateStaticParams() {
   try {
     const countryCodes = await listRegions().then((regions) =>
@@ -34,14 +42,23 @@ export async function generateStaticParams() {
       .filter((param) => param.handle)
   } catch (error) {
     console.error(
-      `Failed to generate static paths for product pages: ${
-        error instanceof Error ? error.message : "Unknown error"
+      `Failed to generate static paths for product pages: ${error instanceof Error ? error.message : "Unknown error"
       }.`
     )
     return []
   }
 }
 
+/**
+ * 
+ * @param props - Props
+ * @returns 
+ * Purpose: This function is used to dynamically generate metadata (e.g., <title>, <meta> tags) for the page.
+    When it runs: It is executed on the server before rendering the page to provide metadata for the <head> section.
+    Why it's implemented:
+    It allows the page to have dynamic metadata based on the route parameters or fetched data (e.g., product title, description, and thumbnail).
+    This is useful for SEO and social sharing.
+ */
 export async function generateMetadata(props: Props): Promise<Metadata> {
   const params = await props.params
   const { handle } = params
